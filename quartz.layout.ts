c59b5d668bd966d99ec5bcbe71effe291a2cb177
Devansh Mahajan <mahajandevansh@icloud.com>
2025-09-14 20:50:21 +0200

    Remove Discord and GitHub links from footer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,10 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   afterBody: [Comments],
   footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
-    },
+    links: {},
   }),
 }
 
