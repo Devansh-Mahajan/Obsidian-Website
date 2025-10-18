b42b1263027940373ac84ddaa96be0a4f8b4d5a9
Devansh Mahajan <mahajandevansh@icloud.com>
2025-10-19 00:06:45 +0200

    Update configuration and styling

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "My Notes",
-    pageTitleSuffix: "",
+    pageTitle: "~/Dash",
+    pageTitleSuffix: " - Devansh Mahajan",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
